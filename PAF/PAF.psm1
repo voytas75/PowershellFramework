e3f8cd416dbf28d1eeb83e11f2e5b9d7f3e1d356
Wojciech Napierała <wnapierala@hotmail.com>
2023-08-03 16:10:17 +0200

    0.2.3

--- PAF/PAF.psm1
+++ PAF/PAF.psm1
@@ -851,7 +851,7 @@ $oldProtocol = [Net.ServicePointManager]::SecurityProtocol
 $ModuleName = "PAF"
 
 # Get the installed version of the module
-$ModuleVersion = [version]"0.2.2"
+$ModuleVersion = [version]"0.2.3"
 
 # Find the latest version of the module in the PSGallery repository
 $LatestModule = Find-Module -Name $ModuleName -Repository PSGallery
